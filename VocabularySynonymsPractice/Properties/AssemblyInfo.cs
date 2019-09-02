a91e5fe0b3483d65d6ce515de67d71b9305dd8e3
Yonatan Mankovich <yonimn2000@gmail.com>
2019-09-01 20:02:23 -0400

    Changed solution and project names

--- VocabularySynonymsPractice/Properties/AssemblyInfo.cs
+++ VocabularySynonymsPractice/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VocabularyKeyWordsQuiz")]
+[assembly: AssemblyTitle("Vocabulary Synonyms Practice")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VocabularyKeyWordsQuiz")]
+[assembly: AssemblyCompany("Yonatan Mankovich")]
+[assembly: AssemblyProduct("YonatanMankovich.VocabularySynonymsPractice")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
